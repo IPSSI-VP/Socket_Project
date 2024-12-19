5c1157e6723a3e0b221bd9874729a38a29051031
Comete419 <lucasavona1999@gmail.com>
2024-12-19 11:26:00 +0100

    generation aléatoire

---
+++ src/gener.h
@@ -0,0 +1,7 @@
+#ifndef GENER_H  // Protéger contre les inclusions multiples
+#define GENER_H
+
+// Déclaration de la fonction qui génère une chaîne aléatoire
+char* gener(unsigned int length);
+
+#endif  // GENER_H
